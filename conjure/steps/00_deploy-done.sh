5728d5c4187219bc61de6c7cad96b32c3161d498
Adam Stokes <adam.stokes@ubuntu.com>
2016-06-09 08:01:18 -0400

    update bundle, deploy-done
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- conjure/steps/00_deploy-done.sh
+++ conjure/steps/00_deploy-done.sh
@@ -7,6 +7,18 @@ services=("haproxy" \
           "ghost")
 
 checkUnitsForErrors $services
-checkUnitsForActive $services
+
+if [ $(unitStatus ghost 0) != "active" ]; then
+    exposeResult "Waiting on ghost" 0 "false"
+fi
+
+if [ $(unitStatus mysql 0) != "active" ]; then
+    exposeResult "Waiting on mysql" 0 "false"
+fi
+
+if [ $(unitStatus haproxy 0) != "active" ]; then
+    exposeResult "Waiting on haproxy" 0 "false"
+fi
+
 
 exposeResult "Applications ready" 0 "true"
